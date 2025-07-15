1c1f7210701d07acf3ba8a4614c13e26a8d1bac6
XBigDanX <daki.lozic12@gmail.com>
2025-07-15 12:49:49 +0200

    test2

--- database/migrations/2025_07_15_081622_create_products_table.php
+++ database/migrations/2025_07_15_081622_create_products_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    
+
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
@@ -25,6 +25,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
+    
     public function down(): void
     {
         Schema::dropIfExists('products');
